4d15abf2bd54c9fda09f4631eeefa96b26343dd6
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2024-04-25 14:54:16 +0530

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/",
+        url: "https://microservice-chl9.onrender.com",
         description: "Live server",
       },
     ],
